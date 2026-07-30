bb5c09f39a9861425a2b08c28acf1be8960dcd4d
Omar Bonilla <omar.bonilla@timia.ai>
2026-07-30 18:08:16 -0500

    Limpieza

--- src/Code.js
+++
@@ -1,5 +0,0 @@
-function doGet() {
-  return HtmlService.createHtmlOutputFromFile('Index')
-      .setTitle('Mi Web App con CI/CD')
-      .setXframeOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
-}
